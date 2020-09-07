b08db360fe261dafe0c9b67a530cadbd20fff197
Larry Golding <lgolding@comcast.net>
2020-09-07 07:44:05 -0700

    Introduce DropZone label parameter.

---
+++ SarifWorld.ComponentsLibrary/DropZone.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SarifWorld.ComponentsLibrary
+{
+    public partial class DropZone
+    {
+        [Parameter]
+        public string Label { get; set; } = string.Empty;
+    }
+}
