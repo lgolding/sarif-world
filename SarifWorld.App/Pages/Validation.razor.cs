7db6c65c712ceec721bec64708f37ae0a836f510
Larry Golding <larrygolding@comcast.net>
2020-09-08 12:12:17 -0700

    Remove dummy dropped file handling.

--- SarifWorld.App/Pages/Validation.razor.cs
+++ SarifWorld.App/Pages/Validation.razor.cs
@@ -4,19 +4,8 @@ namespace SarifWorld.App.Pages
 {
     public partial class Validation
     {
-        private const int HeadLength = 50;
-
-        private int NumFiles { get; set; } = 0;
-        private string Name { get; set; } = string.Empty;
-        private string Head { get; set; } = string.Empty;
-
         public void HandleDroppedFile(DroppedFile droppedFile)
         {
-            NumFiles++;
-            Name = droppedFile.Name;
-            Head = droppedFile.Text.Length <= HeadLength
-                ? droppedFile.Text
-                : droppedFile.Text.Substring(0, HeadLength) + "\u2026";
         }
     }
 }
