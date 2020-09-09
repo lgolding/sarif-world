ece3a709ec353fbe072617b8215ab978ef353d53
Larry Golding <larrygolding@comcast.net>
2020-09-08 20:48:28 -0700

    Change to server app

--- SarifWorld.App/Program.cs
+++ SarifWorld.App/Program.cs
@@ -1,37 +1,20 @@
-using System;
-using System.Globalization;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.CodeAnalysis.Sarif;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.JSInterop;
-using SarifWorld.App.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace SarifWorld.App
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddScoped<ISarifValidationService, SarifValidationService>();
-            builder.Services.AddScoped<IFileSystem, FileSystem>();
-            builder.Services.AddLocalization();
-
-            WebAssemblyHost host = builder.Build();
-
-            var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
-            var language = await jsInterop.InvokeAsync<string>("getLanguage");
-
-            var culture = new CultureInfo(language);
-            CultureInfo.DefaultThreadCurrentCulture = culture;
-            CultureInfo.DefaultThreadCurrentUICulture = culture;
-
-            await host.RunAsync();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
