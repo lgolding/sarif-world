4a2eea7db14ca4099dc8a6a3accdb9334d40d4f0
Larry Golding <larrygolding@comcast.net>
2020-09-11 14:44:38 -0700

    Add first DropZone unit test.

---
+++ SarifWorld.ComponentsLibrary.UnitTests/DropZoneTests.cs
@@ -0,0 +1,26 @@
+﻿using Bunit;
+using Bunit.TestDoubles.JSInterop;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using Xunit;
+
+namespace SarifWorld.ComponentsLibrary.UnitTests
+{
+    public class DropZoneTests : TestContext
+    {
+        [Fact]
+        public void DropZone_HasDefaultLabels()
+        {
+            Services.AddLogging();
+            Services.AddLocalization();
+            Services.AddMockJSRuntime();
+
+            IRenderedComponent<DropZone> cut = RenderComponent<DropZone>();
+            DropZone dropZone = cut.Instance;
+
+            IStringLocalizer<DropZone> localizer = Services.GetService<IStringLocalizer<DropZone>>();
+            dropZone.DefaultLabel.Should().Be(localizer["DefaultLabel"]);
+        }
+    }
+}
