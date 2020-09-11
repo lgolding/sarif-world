be03cded1d2f3446d83f4ae3864b8e2bbb48aa85
Larry Golding <larrygolding@comcast.net>
2020-09-11 13:13:10 -0700

    Introduce empty App unit tests projects.

---
+++ SarifWorld.App.UnitTests/ValidationTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace SarifWorld.App.UnitTests
+{
+    public class ValidationTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
