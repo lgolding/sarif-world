4de9273fc821cd38fbf85033e4ba1d60eb47fda1
Larry Golding <larrygolding@comcast.net>
2020-09-11 10:19:27 -0700

    Introduce Alert.Message.

--- SarifWorld.ComponentsLibrary/Alert.razor.cs
+++ SarifWorld.ComponentsLibrary/Alert.razor.cs
@@ -1,6 +1,10 @@
-﻿namespace SarifWorld.ComponentsLibrary
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SarifWorld.ComponentsLibrary
 {
     public partial class Alert
     {
+        [Parameter]
+        public string Message { get; set; } = string.Empty;
     }
 }
