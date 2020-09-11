956af4312532a0c64ea55ae0c255f769d75ff58a
Larry Golding <larrygolding@comcast.net>
2020-09-11 13:02:51 -0700

    Display rule ids in Multiselect.

--- SarifWorld.App/Pages/Validation.razor.cs
+++ SarifWorld.App/Pages/Validation.razor.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+using Microsoft.CodeAnalysis.Sarif;
+using Microsoft.TeamFoundation.Common;
 using SarifWorld.App.Models;
 using SarifWorld.App.Services;
 using SarifWorld.ComponentsLibrary;
@@ -12,17 +16,40 @@ namespace SarifWorld.App.Pages
 
         public Alert Alert { get; set; }
 
+        public Multiselect RulesSelector { get; set; }
+
         public void ValidateDroppedFile(DroppedFile droppedFile)
         {
             ValidationResult validationResult = SarifValidationService.ValidateFile(droppedFile.Name, droppedFile.Text);
             if (string.IsNullOrEmpty(validationResult.ErrorMessage))
             {
                 Alert.ShowMessage($"Number of results: {validationResult.ValidationLog.Runs[0].Results.Count}");
+                List<string> ruleIds = GetRuleIds(validationResult.ValidationLog);
+                RulesSelector.SetOptions(ruleIds);
             }
             else
             {
                 Alert.ShowError(validationResult.ErrorMessage);
             }
+        }
+
+        List<string> GetRuleIds(SarifLog log)
+        {
+            IList<Run> runs = log.Runs;
+            if (runs?.Any() == true)
+            {
+                Run run = runs[0];
+                IList<ReportingDescriptor> rules = run?.Tool?.Driver?.Rules;
+                if (rules?.Any() == true)
+                {
+                    return rules
+                        .Select(r => r.Id)
+                        .Where(s => !s.IsNullOrEmpty())
+                        .ToList();
+                }
+            }
+
+            return new List<string>();
         }
     }
 }
---
+++ SarifWorld.ComponentsLibrary/Multiselect.razor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace SarifWorld.ComponentsLibrary
+{
+    public partial class Multiselect
+    {
+        [Parameter]
+        public List<string> Options { get; set; } = new List<string>();
+
+        public void SetOptions(List<string> options)
+        {
+            Options = options;
+            StateHasChanged();
+        }
+    }
+}
