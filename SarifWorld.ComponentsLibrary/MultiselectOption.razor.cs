0c313e49260595b6f07c231665480bad02a84888
Larry Golding <larrygolding@comcast.net>
2020-09-11 17:47:16 -0700

    Add checkboxes to Multiselect UI.

---
+++ SarifWorld.ComponentsLibrary/MultiselectOption.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SarifWorld.ComponentsLibrary
+{
+    public partial class MultiselectOption
+    {
+        [Parameter]
+        public string Label { get; set; } = string.Empty;
+    }
+}
