fca568b15e661e5a75e0cffa75debc390e1c094c
Larry Golding <lgolding@comcast.net>
2020-09-19 12:26:00 -0700

    Add a Selenium smoke test.

---
+++ SarifWorld.UITests/IndexPageTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace SarifWorld.UITests
+{
+    public class IndexPageTests
+    {
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void IndexPage_ShouldBeDisplayedWhenAppStarts()
+        {
+            // Note: If you run these tests with VS running as Admin,
+            // Chrome crashes (displays a frowny face) and the test
+            // hangs and never finishes. I searched and found several
+            // people who observed this, and the thing that most say
+            // is to run the tests non-Admin. I don't know the root cause.
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://localhost:44392/");
+            }
+        }
+    }
+}
