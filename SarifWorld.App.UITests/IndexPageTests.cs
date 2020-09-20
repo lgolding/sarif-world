05a70aa1b6de0bb172441e73cd82840bdc8d5f8c
Larry Golding <lgolding@comcast.net>
2020-09-20 14:53:14 -0700

    Factor out PageTestBase.

--- SarifWorld.App.UITests/IndexPageTests.cs
+++ SarifWorld.App.UITests/IndexPageTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using FluentAssertions;
-using OpenQA.Selenium;
-using OpenQA.Selenium.Chrome;
+﻿using FluentAssertions;
 using SarifWorld.TestUtilities;
 using Xunit;
 
@@ -11,53 +8,16 @@ namespace SarifWorld.App
     // snap!". Multiple posters report this and suggest running the tests as non-Admin, but
     // I don't know the root cause.
     [Trait("Category", "UITest")]
-    public class IndexPageTests : IDisposable
+    public class IndexPageTests : PageTestBase
     {
-        private IWebDriver driver;
-        private bool disposed;
-
-        public IndexPageTests()
-        {
-            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
-            service.LogPath = "chromedriver.log";
-            service.EnableVerboseLogging = true;
-
-            this.driver = new ChromeDriver(service);
-            this.driver.Navigate().GoToUrl("http://localhost:61981/");
-        }
-
         [Fact]
         [Trait("Category", "Smoke")]
-        public void IndexPage_ShouldHaveCorrectTitle()
+        public void IndexPage_ShouldHaveCorrectTitleAndUrl()
         {
             var stringResources = new ResourceStrings(typeof(Pages.Index));
             string expectedTitle = stringResources["PageTitle"];
 
-            this.driver.Title.Should().Be(expectedTitle);
-        }
-
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposed)
-            {
-                if (disposing)
-                {
-                    if (this.driver != null)
-                    {
-                        this.driver.Dispose();
-                        this.driver = null;
-                    }
-                }
-
-                disposed = true;
-            }
-        }
-
-        public void Dispose()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
+            Driver.Title.Should().Be(expectedTitle);
         }
     }
 }
---
+++ SarifWorld.App.UITests/PageTestBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SarifWorld.App
+{
+    public abstract class PageTestBase : IDisposable
+    {
+        private bool disposed;
+
+        protected PageTestBase()
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.LogPath = "chromedriver.log";
+            service.EnableVerboseLogging = true;
+
+            Driver = new ChromeDriver(service);
+            Driver.Navigate().GoToUrl("http://localhost:61981/");
+        }
+
+        protected IWebDriver Driver { get; private set; }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (Driver != null)
+                    {
+                        Driver.Dispose();
+                        Driver = null;
+                    }
+                }
+
+                disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
