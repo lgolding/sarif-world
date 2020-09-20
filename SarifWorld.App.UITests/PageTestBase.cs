cf9343fcce87c1f122ba18a9d5e49e166a70f3f7
Larry Golding <lgolding@comcast.net>
2020-09-20 15:21:16 -0700

    Get application URI from launchSettings; don't hard-code in test.

--- SarifWorld.App.UITests/PageTestBase.cs
+++ SarifWorld.App.UITests/PageTestBase.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Json.Pointer;
+using Newtonsoft.Json.Linq;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
@@ -14,13 +17,15 @@ namespace SarifWorld.App
             service.LogPath = "chromedriver.log";
             service.EnableVerboseLogging = true;
 
+            ApplicationUri = GetApplicationUri();
+
             Driver = new ChromeDriver(service);
             Driver.Navigate().GoToUrl(ApplicationUri);
         }
 
         protected IWebDriver Driver { get; private set; }
 
-        protected string ApplicationUri { get; } = "https://localhost:44392/";
+        protected string ApplicationUri { get; }
 
         protected virtual void Dispose(bool disposing)
         {
@@ -44,6 +49,33 @@ namespace SarifWorld.App
             // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
             Dispose(disposing: true);
             GC.SuppressFinalize(this);
+        }
+
+        private string GetApplicationUri()
+        {
+            const string LaunchSettingsPath = @"Properties\launchSettings.json";
+            const string IisExpressSettingsPointerString = "/iisSettings/iisExpress";
+            const string ApplicationUriScheme = "https:";
+
+            string applicationUrlPointerString = $"{IisExpressSettingsPointerString}/applicationUrl";
+            string sslPortPointerString = $"{IisExpressSettingsPointerString}/sslPort";
+
+            string documentText = File.ReadAllText(LaunchSettingsPath);
+            JToken documentToken = JToken.Parse(documentText);
+
+            var applicationUriPointer = new JsonPointer(applicationUrlPointerString);
+            JToken applicationUriToken = applicationUriPointer.Evaluate(documentToken);
+            var applicationUriString = (string)applicationUriToken;
+
+            var sslPortPointer = new JsonPointer(sslPortPointerString);
+            JToken sslPortToken = sslPortPointer.Evaluate(documentToken);
+            var sslPort = (int)sslPortToken;
+
+            var builder = new UriBuilder(applicationUriString);
+            builder.Scheme = ApplicationUriScheme;
+            builder.Port = sslPort;
+
+            return builder.ToString();
         }
     }
 }
